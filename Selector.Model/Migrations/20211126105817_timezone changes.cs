975ee772ddc1b85128838e748f5d3970c048f9d6
andy <andy@sarsoo.xyz>
2021-11-26 20:40:18 +0000

    adding date time with timezone migration

---
+++ Selector.Model/Migrations/20211126105817_timezone changes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Selector.Model.Migrations
+{
+    public partial class timezonechanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SpotifyLastRefresh",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "00c64c0a-3387-4933-9575-83443fa9092b",
+                column: "ConcurrencyStamp",
+                value: "0801d9f2-0f90-4ca7-bb85-eaa36046fc86");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SpotifyLastRefresh",
+                table: "AspNetUsers",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "00c64c0a-3387-4933-9575-83443fa9092b",
+                column: "ConcurrencyStamp",
+                value: "90334695-e16c-4c28-b492-e267945d55e8");
+        }
+    }
+}
