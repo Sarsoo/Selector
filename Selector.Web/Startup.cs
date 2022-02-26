64b7105514d5ea3c2ac69f9aa4bd6498c262a31f
andy <andy@sarsoo.xyz>
2022-02-26 21:50:49 +0000

    skipping https redirect for production

--- Selector.Web/Startup.cs
+++ Selector.Web/Startup.cs
@@ -129,7 +129,7 @@ namespace Selector.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                app.UseHttpsRedirection();
+                // app.UseHttpsRedirection();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
             }
