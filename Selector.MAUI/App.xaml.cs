24fca12366f0e1f40a497bda9f6db0b0c266ba0b
Andy Pack <andy@sarsoo.xyz>
2023-01-21 16:55:51 +0000

    adding MAUI skeleton

---
+++ Selector.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Selector.MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ Selector.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Selector.MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
