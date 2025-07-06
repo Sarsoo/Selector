5f9a6b7519eaec35ae2f28a8cca9968765cb2ab6
Andy Pack <andy@sarsoo.xyz>
2025-07-06 18:32:37 +0100

    single docker build action

--- Selector.MAUI/MauiJsonContext.cs
+++ Selector.MAUI/MauiJsonContext.cs
@@ -4,6 +4,7 @@ using Selector.MAUI.Services;
 namespace Selector.MAUI;
 
 [JsonSerializable(typeof(SelectorNetClient.TokenNetworkResponse))]
+[JsonSourceGenerationOptions(PropertyNameCaseInsensitive = true)]
 public partial class MauiJsonContext : JsonSerializerContext
 {
 }
